8367a75ecb0d3f988fcffb04c877d901979a59fd
Jacob Ferrero <jacobaferrero@gmail.com>
2017-10-22 21:08:32 -0500

    Adds example Gateway files

---
+++ shared/src/main/java/com/queatz/snappy/shared/Gateway_example.java
@@ -0,0 +1,12 @@
+package com.queatz.snappy.shared;
+
+// Rename to: Gateway.java
+
+public class Gateway_example {
+    public static final String projectId = "<example>";
+    public static final String clientId = "<example>.apps.googleusercontent.com";
+    public static final String gcmKey = "<example>";
+    public static final String ARANGO_USER = "snappy";
+    public static final String ARANGO_PASSWORD = "snappy";
+    public static final String VILLAGE_EMAIL_PASSWORD = "<example>";
+}
